35e301750b7da95edf24eaf98c465b475d95545f
clyde <ivdone@clydedeMacBook-Pro.local>
2018-08-14 17:40:55 +0800

    add qr

--- site/wanglai-web-react-express/src/Components/Footer.js
+++ site/wanglai-web-react-express/src/Components/Footer.js
@@ -6,6 +6,7 @@ class Footer extends Component {
       <footer className="footer-section spad">
         <div className="container text-center">
           <div className="social">
+            <a><img className="img-fluid" src="/img/contact/Wechat-QR.jpeg" alt=""/></a>
             <a><i className="fa fa-pinterest"></i></a>
             <a><i className="fa fa-facebook"></i></a>
             <a><i className="fa fa-twitter"></i></a>
